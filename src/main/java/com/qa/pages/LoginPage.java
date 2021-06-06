ce5b43790c64a7737335034b4151ae34c8fdb712
Sanchita <aru.sanchita@gmail.com>
2021-06-07 00:22:01 +0530

    Second

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,12 @@
+package com.qa.pages;
+
+public class LoginPage {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Login Page");
+
+	}
+
+}
--- src/main/java/com/qa/test/Test.java
+++ src/main/java/com/qa/test/Test.java
@@ -7,6 +7,7 @@ public class Test {
 
 		
 		System.out.println("Hello Git");
+		System.out.println("Hello Worlds");
 	}
 
 }
